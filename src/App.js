c41b3e4a81cd1ef8e0ee177c2958621191cac7df
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-04-27 14:21:58 -0700

    app structure in place

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <nav>
+        <h3> FRIENDS </h3>
+        <button><Link to="/login">LOGIN</Link></button>
+        <button><Link to="/list">FRIEND LIST</Link></button>
+        <button><Link to="/addfriend">ADD FRIEND.</Link></button>
+        <button><Link to="/logout">LOGOUT</Link></button>
+      </nav>
+      <div id="login">
+        <form>
+
+          <label> USERNAME
+          <input
+            type="text"
+          />
+          </label>
+          
+          <label> PASSWORD
+            <input
+              type="password"
+            />
+          </label>
+          
+          <button>submit</button>
+        </form>
+      </div>
     </div>
   );
 }
