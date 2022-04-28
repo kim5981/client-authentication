3c27da52689d9f0534fd496419461970c7911064
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-04-27 18:14:47 -0700

    applied styling to login page

--- src/App.js
+++ src/App.js
@@ -2,32 +2,44 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
 function App() {
+
+  const labelStyle ={
+    fontWeight: "bold"
+  }
   return (
     <div className="App">
+      <div className='nav'>
+      <h3> friends database </h3>
       <nav>
-        <h3> FRIENDS </h3>
-        <button><Link to="/login">LOGIN</Link></button>
-        <button><Link to="/list">FRIEND LIST</Link></button>
-        <button><Link to="/addfriend">ADD FRIEND.</Link></button>
-        <button><Link to="/logout">LOGOUT</Link></button>
+        <a href="/login">login</a>
+        <a href="/list">friend list</a>
+        <a href="/addfriend">add friend</a>
+        <a href="/logout">logout</a>
       </nav>
+      </div>
+      
       <div id="login">
+        <h1> LOGIN </h1>
         <form>
 
-          <label> USERNAME
+          <label style={ labelStyle }> 
+          USERNAME
           <input
             type="text"
           />
           </label>
           
-          <label> PASSWORD
+          <label style={ labelStyle }> 
+          PASSWORD
             <input
               type="password"
             />
           </label>
           
-          <button>submit</button>
+          <button className="loginBtn">SUBMIT</button>
         </form>
       </div>
     </div>
